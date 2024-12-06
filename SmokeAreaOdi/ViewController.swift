c753d2389d10f13c0dbedaaf121f15365d460730
23ji <isj4194@naver.com>
2024-12-06 17:49:24 +0900

    스토리 보드로 전환, 지도 뷰 추가, 내 위치 버튼 추가

--- SmokeAreaOdi/ViewController.swift
+++ SmokeAreaOdi/ViewController.swift
@@ -1,11 +1,50 @@
 import NMapsMap
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-  override func viewDidLoad() {
-      super.viewDidLoad()
-      //네이버 지도 화면 띄우기
-      let mapView = NMFMapView(frame: view.frame)
-      view.addSubview(mapView)
-  }
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var mapView: NMFMapView!
+//    let locationManager = CLLocationManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      // setupMapView()
+//        setupLocationManager()
+    }
+
+//    func setupMapView() {
+//        mapView = NMFMapView(frame: view.frame)
+//        view.addSubview(mapView)
+//    }
+//
+//    func setupLocationManager() {
+//        locationManager.delegate = self
+//        locationManager.requestWhenInUseAuthorization() // 위치 권한 요청
+//        locationManager.startUpdatingLocation() // 위치 업데이트 시작
+//    }
+//
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let location = locations.first else { return }
+//        let lat = location.coordinate.latitude
+//        let lng = location.coordinate.longitude
+//
+//        // 지도 중심 이동
+//        let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: lat, lng: lng))
+//        mapView.moveCamera(cameraUpdate)
+//
+//        // 현재 위치 마커 표시
+//        let marker = NMFMarker()
+//        marker.position = NMGLatLng(lat: lat, lng: lng)
+//        marker.mapView = mapView
+//    }
+//
+//    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+//        print("위치 정보를 가져오지 못했습니다: \(error.localizedDescription)")
+//    }
 }
+
+
+
