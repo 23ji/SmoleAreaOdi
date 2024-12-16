d840607a18101eeccabbb9a82cfe5972bfefad66
23ji <isj4194@naver.com>
2024-12-16 14:47:20 +0900

    기본 지도 뷰 표현 및 주석 설명 추가

--- SmokeAreaOdi/ViewController.swift
+++ SmokeAreaOdi/ViewController.swift
@@ -1,50 +1,21 @@
-import NMapsMap
-import UIKit
-import CoreLocation
+import CoreLocation //GPS와 관련된 위치 정보를 다룰 수 있도록 하는 Apple의 프레임워크
+import NMapsMap //네이버 지도 SDK로, 네이버 지도를 앱에 표시하거나 사용자 위치를 관리할 때 사용
 
-
-class ViewController: UIViewController, CLLocationManagerDelegate {
+        // 앱 화면에 지도를 표시할 메인 뷰 컨트롤러
+class MainViewController: UIViewController, CLLocationManagerDelegate {
     
-    var mapView: NMFMapView!
-//    let locationManager = CLLocationManager()
-    
+    // 위치 관리자 초기화 / CLLocationManager 객체를 생성 / 이 객체를 통해 GPS 정보를 가져오고, 위치 권한 요청 등을 처리할 수 있음.
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      // setupMapView()
-//        setupLocationManager()
+        
+        //현재 뷰 컨트롤러의 크기와 동일한 크기로 초기화
+        let naverMapView = NMFNaverMapView(frame: view.frame)
+        
+        //네이버 지도 뷰를 현재 ViewController의 뷰 계층에 추가하여 화면에 보이게 함.
+        view.addSubview(naverMapView)
+        
+        
     }
-
-//    func setupMapView() {
-//        mapView = NMFMapView(frame: view.frame)
-//        view.addSubview(mapView)
-//    }
-//
-//    func setupLocationManager() {
-//        locationManager.delegate = self
-//        locationManager.requestWhenInUseAuthorization() // 위치 권한 요청
-//        locationManager.startUpdatingLocation() // 위치 업데이트 시작
-//    }
-//
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        guard let location = locations.first else { return }
-//        let lat = location.coordinate.latitude
-//        let lng = location.coordinate.longitude
-//
-//        // 지도 중심 이동
-//        let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: lat, lng: lng))
-//        mapView.moveCamera(cameraUpdate)
-//
-//        // 현재 위치 마커 표시
-//        let marker = NMFMarker()
-//        marker.position = NMGLatLng(lat: lat, lng: lng)
-//        marker.mapView = mapView
-//    }
-//
-//    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-//        print("위치 정보를 가져오지 못했습니다: \(error.localizedDescription)")
-//    }
 }
-
-
-
